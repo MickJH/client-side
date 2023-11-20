6fba3ce2b55163c170aed4f37763c3bad9c9a1a3
MickJH <mj.holster@student.avans.nl>
2023-11-20 23:50:44 +0100

    Backend tests now passing as well

--- apps/h-cars/src/auth/auth.controller.spec.ts
+++ apps/h-cars/src/auth/auth.controller.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
-
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { UserService } from '../user/user.service';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        JwtAuthGuard,
+        UserService,
+        {
+          provide: getModelToken('User'),
+          useValue: {}, 
+        },
+      ],
+      imports: [UserModule, MongooseModule.forRoot(process.env.MONGO_URI)],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
