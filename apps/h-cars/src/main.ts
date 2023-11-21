9e59a1f2ae72569f9d5547ec6fc70e50f3ad566a
MickJH <mj.holster@student.avans.nl>
2023-11-21 17:27:10 +0100

    Enable error messages for better debugging

--- apps/h-cars/src/main.ts
+++ apps/h-cars/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -8,12 +8,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(passport.initialize());
+  app.useGlobalPipes(new ValidationPipe()); // Enable global validation pipe
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        disableErrorMessages: false, // Enable detailed error messages
+      }),
+    );
+  
+
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
