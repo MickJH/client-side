ff3c7d3a3620f83c88dc1aa8572b38cf7fbc96f1
MickJH <mj.holster@student.avans.nl>
2023-11-23 22:42:24 +0100

    Fixed Product API Errors

--- apps/h-cars/src/app/app.module.ts
+++ apps/h-cars/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '../user/user.module';
 import { CarModule } from '../car/car.module';
+import { ProductModule } from '../product/product.module';
 import { CarController } from '../car/car.controller';
 import { JwtService } from '@nestjs/jwt';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { AuthController } from '../auth/auth.controller';
 import { AuthService } from '../auth/auth.service';
 import { ProductController } from '../product/product.controller';
+import { CarService } from '../car/car.service';
+import { ProductService } from '../product/product.service';
 
 @Module({
   imports: [
@@ -19,6 +22,7 @@ import { ProductController } from '../product/product.controller';
     MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
     CarModule,
+    ProductModule,
   ],
   controllers: [
     AppController,
