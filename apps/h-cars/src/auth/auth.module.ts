10db087deac14d81863758f98e3e6d9f0bf3d333
MickJH <mj.holster@student.avans.nl>
2023-11-30 17:24:46 +0100

    Fixed application not building

--- apps/h-cars/src/auth/auth.module.ts
+++ apps/h-cars/src/auth/auth.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { CarModule } from '../car/car.module';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService, JwtAuthGuard, JwtService],
   controllers: [AuthController],
-  imports: [UserModule, CarModule,],
+  imports: [UserModule],
 })
 export class AuthModule {}
